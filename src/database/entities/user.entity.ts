eca57cf10aa9ba3dadae6a1cfcaba76842bc5fa5
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2026-01-07 18:39:29 +0200

    fix(user): add date_of_birth constraint to ensure valid dates

--- src/database/entities/user.entity.ts
+++ src/database/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import {
+  Check,
   Column,
   Entity,
   JoinColumn,
@@ -21,6 +22,7 @@ export class UserEntity {
   @Column({ name: 'last_name', type: 'varchar', length: 255 })
   lastName: string;
 
+  @Check('"date_of_birth" <= CURRENT_DATE')
   @Column({ name: 'date_of_birth', type: 'date' })
   dateOfBirth: Date;
 
