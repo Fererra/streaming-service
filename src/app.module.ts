15cc4df79ed236f4a2c9de872316c63e695e2d4e
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2026-01-10 13:25:31 +0200

    feat(country): implement country module with controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { CountryModule } from './modules/country/country.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     AuthModule,
+    CountryModule,
   ],
 })
 export class AppModule {}
