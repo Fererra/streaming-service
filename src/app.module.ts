c64a8753215d4e98ed337f14199aa5e4704d783a
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2026-01-11 17:13:06 +0200

    feat(app): add @nestjs/schedule and integrate ScheduleModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { CountryModule } from './modules/country/country.module';
+import { TasksModule } from './modules/tasks/tasks.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
@@ -9,8 +11,10 @@ import { CountryModule } from './modules/country/country.module';
       envFilePath: '.env.development.local',
       isGlobal: true,
     }),
+    ScheduleModule.forRoot(),
     AuthModule,
     CountryModule,
+    TasksModule,
   ],
 })
 export class AppModule {}
