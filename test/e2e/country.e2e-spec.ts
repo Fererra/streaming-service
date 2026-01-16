5c19e24794f3a0595de4f12ccbc8f76dd62073f1
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2026-01-16 20:12:41 +0200

    test(e2e): add end-to-end tests for AdminUsers, Auth and Country controllers

---
+++ test/e2e/country.e2e-spec.ts
@@ -0,0 +1,32 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { AppModule } from 'src/app.module';
+
+describe('CountryController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('GET /countries', () => {
+    it('200 + array ofall countries', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/countries')
+        .expect(200);
+
+      expect(Array.isArray(response.body)).toBe(true);
+    });
+  });
+});
