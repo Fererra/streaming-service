b595520bad61726febddf07e271a345401cf81a4
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2026-01-24 17:12:27 +0200

    feat(persons): create PersonEntity and migration for persons table

---
+++ src/database/entities/person.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Check,
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { CountryEntity } from './country.entity';
+
+@Entity('persons')
+export class PersonEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'first_name', type: 'varchar', length: 255 })
+  firstName: string;
+
+  @Column({ name: 'last_name', type: 'varchar', length: 255 })
+  lastName: string;
+
+  @Column({ name: 'photo_path', type: 'varchar', length: 255, nullable: true })
+  photoPath: string | null;
+
+  @Check('"date_of_birth" <= CURRENT_DATE')
+  @Column({ name: 'date_of_birth', type: 'date' })
+  dateOfBirth: Date;
+
+  @Column({ type: 'text', nullable: true })
+  biography: string | null;
+
+  @ManyToOne(() => CountryEntity, (country) => country.persons)
+  @JoinColumn({ name: 'country_code' })
+  country: CountryEntity;
+}
