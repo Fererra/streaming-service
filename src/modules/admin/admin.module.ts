9c0ef31bff8ad783baf3a9fe8ee079086bb0bd7e
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2026-01-25 19:32:11 +0200

    feat(admin): implement AdminPersonsController for managing persons

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminUsersController } from './admin-users.controller';
 import { UsersModule } from '../users/users.module';
+import { PersonsModule } from '../persons/persons.module';
+import { AdminPersonsController } from './admin-persons.controller';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AdminUsersController],
+  imports: [UsersModule, PersonsModule],
+  controllers: [AdminUsersController, AdminPersonsController],
 })
 export class AdminModule {}
