f6a5b1e2207e5944067f91f4d168a1d5b0384495
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2026-01-27 16:33:21 +0200

    feat(user): add avatar_path column to UserEntity and corresponding migration

--- src/database/entities/user.entity.ts
+++ src/database/entities/user.entity.ts
@@ -22,6 +22,9 @@ export class UserEntity {
   @Column({ name: 'last_name', type: 'varchar', length: 255 })
   lastName: string;
 
+  @Column({ name: 'avatar_path', type: 'varchar', length: 255, nullable: true })
+  avatarPath: string | null;
+
   @Check('"date_of_birth" <= CURRENT_DATE')
   @Column({ name: 'date_of_birth', type: 'date' })
   dateOfBirth: Date;
