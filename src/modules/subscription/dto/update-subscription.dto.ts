027bf36c8a43ebd85b2b8c072191bae5ce60a7ef
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2026-02-08 18:52:39 +0200

    feat(subscriptions): add UpdateOfferDto for partial updates to offers

--- src/modules/subscription/dto/update-subscription.dto.ts
+++ src/modules/subscription/dto/update-subscription.dto.ts
@@ -1,6 +1,11 @@
 import { OmitType, PartialType } from '@nestjs/mapped-types';
-import { CreateSubscriptionDto } from './create-subscription.dto';
+import {
+  CreateOfferDto,
+  CreateSubscriptionDto,
+} from './create-subscription.dto';
 
 export class UpdateSubscriptionDto extends PartialType(
   OmitType(CreateSubscriptionDto, ['offers'] as const),
 ) {}
+
+export class UpdateOfferDto extends PartialType(CreateOfferDto) {}
