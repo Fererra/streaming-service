8d156c30105771b4d08342f8d65699b5dbe66f61
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2026-02-26 14:57:16 +0200

    fix(tests): update moduleNameMapper in Jest configuration for improved path resolution

--- apps/api/test/jest-unit.js
+++ apps/api/test/jest-unit.js
@@ -7,4 +7,11 @@ export default {
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   roots: ['<rootDir>/test/unit'],
+  moduleNameMapper: {
+    ...baseConfig.moduleNameMapper,
+    '^@app/config(.*)$': '<rootDir>/../../libs/config/src$1',
+    '^@app/payment(.*)$': '<rootDir>/../../libs/payment/src$1',
+    '^@app/user-subscription(.*)$':
+      '<rootDir>/../../libs/user-subscription/src$1',
+  },
 };
