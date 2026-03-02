d34b33d691fbd902d710021d881938b1a6dda9fe
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2026-03-02 10:00:53 +0200

    feat(repository): update IGatewayPriceRepository to use PaymentGatewayProvider type for gateway parameter

--- apps/api/src/database/repositories/interfaces/gateway-price-repository.interface.ts
+++ apps/api/src/database/repositories/interfaces/gateway-price-repository.interface.ts
@@ -1,13 +1,14 @@
 import { SubscriptionOfferGatewayPriceEntity } from '../../entities/gateway-price.entity';
+import { PaymentGatewayProvider } from '@app/payment';
 
 export interface IGatewayPriceRepository {
   createGatewayPrice(
-    gateway: string,
+    gateway: PaymentGatewayProvider,
     externalPriceId: string,
     offerId: string,
   ): Promise<void>;
   findByOfferIdAndGateway(
     offerId: string,
-    gateway: string,
+    gateway: PaymentGatewayProvider,
   ): Promise<SubscriptionOfferGatewayPriceEntity | null>;
 }
