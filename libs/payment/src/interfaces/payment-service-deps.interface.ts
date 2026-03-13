a895095174dffec654c6f2d943f53590dd0b807f
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2026-03-13 17:53:14 +0200

    refactor(payment): remove unused CreateCheckoutRequest and subscription interfaces

--- libs/payment/src/interfaces/payment-service-deps.interface.ts
+++ libs/payment/src/interfaces/payment-service-deps.interface.ts
@@ -1,20 +1,3 @@
-export class CreateCheckoutRequest {
-  offerId: string;
-}
-
-export interface SubscriptionPlan {
-  id: string;
-  name: string;
-  description: string;
-}
-
-export interface SubscriptionOffer {
-  id: string;
-  price: number;
-  durationMonths: number;
-  subscriptionPlanId: string;
-}
-
 export interface IUserResolver {
   findEmailById(userId: string): Promise<string | null>;
 }
